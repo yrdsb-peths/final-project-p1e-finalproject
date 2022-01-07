8c0aeba9eee8402889a6f9413974a1dc54a2afff
Carl W <335563110@gapps.yrdsb.ca>
2022-01-07 09:27:08 -0500

    Working state

--- MyWorld.java
+++ MyWorld.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+    int x = getWidth()/2;
+    int y = getHeight()/2;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -17,5 +19,11 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1); 
+        GreenfootImage background = new GreenfootImage("Background.jpg");
+        setBackground(background);
+        addObject(new Hedgehog(), x, y);
+        addObject(new Apple(), x, y);
+        addObject(new Platform(), 618, 511);
+        setPaintOrder(Character.class);
     }
 }
---
+++ Platform.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Platform here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Platform extends Actor
+{
+    boolean lever = false;
+    /**
+     * Act - do whatever the Platform wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (!lever) {
+            getImage().scale(getImage().getWidth(), getImage().getHeight()*3);
+            lever = true;
+        }
+    }
+}
