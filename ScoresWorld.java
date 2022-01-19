bfdb7ba35c320fdf7fa80a9c10bc980306c87749
Carl W <335563110@gapps.yrdsb.ca>
2022-01-19 15:24:10 -0500

    Scores world working!

--- ScoresWorld.java
+++ ScoresWorld.java
@@ -13,7 +13,7 @@ public class ScoresWorld extends World
     ArrayList<String> dates;
     String[][] scores;
     int winner = 1; // 0 is tie | 1 is player 1 win | 2 is player 2 win
-    int score = 1; // min * 100 + sec * 10
+    int score; // min * 100 + sec * 10
 
     boolean lever = false;
 
@@ -32,7 +32,28 @@ public class ScoresWorld extends World
     
     private void run() {
         lever = true;
-        ScoreUtilities.writeScore("2000");
-        ScoreUtilities
+        ScoreUtilities.writeScore(Integer.toString(score));
+        ArrayList<String> inputs = ScoreUtilities.extractScores();
+        scores = ScoreUtilities.convertArray(inputs);
+        ScoreUtilities.quickSort(scores);
+        ScoreUtilities.reverse(scores);
+        displaySessionScore();
+        displayScores();
+    }
+
+    private void displayScores() {
+        Label title = new Label("Top 5 High Scores", 100);
+        addObject(title, getWidth()/2, (getHeight()/2)-100);
+        Label underline = new Label("________________", 100);
+        addObject(underline, getWidth()/2, (getHeight()/2)-90);
+        for (int i = 0; i < 5; i++) {
+            Label label = new Label(scores[i][0] + " scored on " + scores[i][1], 75);
+            addObject(label, getWidth()/2, (getHeight()/2) + (i*70));
+        }
+    }
+
+    private void displaySessionScore() {
+        Label l = new Label("Winner's Score: " + Integer.toString(score), 150);
+        addObject(l, getWidth()/2, (getHeight()/2)-250);
     }
 }
