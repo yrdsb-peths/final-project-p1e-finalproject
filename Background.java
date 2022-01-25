a3539b0a99b0c41f19d3288e3028a5fa80f0881b
Carl W <335563110@gapps.yrdsb.ca>
2022-01-25 17:02:43 -0500

    Added falling illusion, stress levels increased

---
+++ Background.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Background here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Background extends Actor
+{
+    public void setScale(Background x) {
+        x.getImage().scale(1000, 600);
+    }
+}
---
+++ MainBackgroundL.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MainBackgroundL here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MainBackgroundL extends Background
+{
+    public MainBackgroundL() {
+        setScale(this);
+    }
+}
