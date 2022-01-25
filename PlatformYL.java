60afd998c04b1ae0b9cbf2b87c53c7dfcdd83cc2
Carl W <335563110@gapps.yrdsb.ca>
2022-01-25 16:42:14 -0500

    Updated game world

--- PlatformYL.java
+++ PlatformYL.java
@@ -8,16 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PlatformYL extends Platform
 {
-    /**
-     * Act - do whatever the PlatformYL wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public PlatformYL()
     {
-        getImage().setTransparency(0);
-    }
-    public void act()
-    {
-        // Add your action code here.
+        setTransparent(this);
     }
 }
--- PlatformYR.java
+++ PlatformYR.java
@@ -8,16 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PlatformYR extends Platform
 {
-    /**
-     * Act - do whatever the PlatformYR wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public PlatformYR()
     {
-        getImage().setTransparency(0);
-    }
-    public void act()
-    {
-        // Add your action code here.
+        setTransparent(this);
     }
 }
